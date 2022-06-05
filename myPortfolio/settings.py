9177adb9afa03b4c5589358a12c7551316e12b3d
Sanja <Sanja42@4b5d82362e5e>
2022-06-05 23:17:26 +0000

    remove cache

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r4$c(x#@=u8(a@%k0467*3^8u6r_)2^bo$0-mh#4bey8f_@+ku'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sanja42.pythonanywhere.com']
 
 
 # Application definition
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR/'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
